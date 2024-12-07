1f271141794905badf90e9fb6108c60feb2608d7
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-12-06 16:04:03 -0800

    added planform imort to chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
